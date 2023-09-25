99bfb74f35f0e50472547a83ced7511a31496100
Trezeguet-tech <djtm.kb@gmail.com>
2023-09-25 13:22:35 -0400

    test12

--- maven-unit-test/src/main/java/com/tkm/examples/MessageBuilder.java
+++ maven-unit-test/src/main/java/com/tkm/examples/MessageBuilder.java
@@ -2,12 +2,12 @@ package com.tkm.examples;
 
 public class MessageBuilder {
 
-    public static String getHelloWorld(){
+    public static String getHelloWorld() {
         return "hello world";
     }
 
-    public static int getNumber10(){
-        return 10;
+    public static int getNumber10() {
+        return 12;
     }
 
 }
