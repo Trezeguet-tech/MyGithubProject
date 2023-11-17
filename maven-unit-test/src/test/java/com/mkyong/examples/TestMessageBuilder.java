6eb4fc5f28dba669fdabd4fd63c95352d9430af8
Trezeguet-tech <djtm.kb@gmail.com>
2023-11-17 15:55:45 -0500

    test tempdir

--- maven-unit-test/src/test/java/com/mkyong/examples/TestMessageBuilder.java
+++ maven-unit-test/src/test/java/com/mkyong/examples/TestMessageBuilder.java
@@ -3,8 +3,15 @@ package com.mkyong.examples;
 import org.junit.jupiter.api.Test;
 
 import com.tkm.examples.MessageBuilder;
+import org.junit.jupiter.api.io.TempDir;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestMessageBuilder {
 
@@ -17,5 +24,23 @@ public class TestMessageBuilder {
     public void testNumber10() {
         assertEquals(30, MessageBuilder.getNumber10());
     }
+
+    @TempDir
+    File anotherTempDir;
+
+    @Test
+    void givenFieldWithTempDirectoryFile_whenWriteToFile_thenContentIsCorrect() throws IOException {
+        assertTrue(this.anotherTempDir.isDirectory(), "Should be a directory ");
+
+        File letters = new File(anotherTempDir, "letters.txt");
+        List<String> lines = Arrays.asList("x", "y", "z");
+
+        Files.write(letters.toPath(), lines);
+
+        assertAll(
+                () -> assertTrue(Files.exists(letters.toPath()), "File should exist"),
+                () -> assertLinesMatch(lines, Files.readAllLines(letters.toPath())));
+    }
+
 
 }
